9de42dd44c1aa66887fedc0856a40dc62a05586d
Sam <77967471+draumaz@users.noreply.github.com>
2022-01-10 20:05:52 -0800

    Delete dots directory

--- dots/lin/prorun.sh
+++
@@ -1,5 +0,0 @@
-export STEAM_COMPAT_DATA_PATH="$HOME/.steam/steam/steamapps/compatdata/$1" 
-export STEAM_COMPAT_CLIENT_INSTALL_PATH="$HOME/.steam/steam" 
-export WINEPREFIX="$HOME/.steam/steam/steamapps/compatdata/$1/pfx"
-"$HOME/.steam/root/steamapps/common/Proton - Experimental/proton" run $2 > /tmp/tmp_prorun 2>&1;
-rm /tmp/tmp_prorun
